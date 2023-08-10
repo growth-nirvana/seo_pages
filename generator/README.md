e878fd842fdd497c60e42ea7e5db2b0d3c5f617e
Juan Ferrari <juannferrari@gmail.com>
2023-08-10 17:16:18 -0300

    rename run file to batch, update readme

--- generator/README.md
+++ generator/README.md
@@ -1,3 +1,8 @@
 # How to run the generator?
 
-TBD
+- Open a terminal in this folder
+- Open ruby shell by typing 'irb' in your terminal (without quotes)
+- Require the batch file by typing
+  require "./batch.rb".
+- Initialize the Batch class and call the #run method
+  Batch.new.run
--- generator/batch.rb
+++ generator/batch.rb
@@ -1,3 +1,21 @@
-# Read from JSON
-# Create a connector landing page for each entry in the JSON
-# Require the Generator class and use it
+require_relative "generator"
+
+# This is the entry point of the generator
+# It reads the connectors.json file and generates a landing page for each connector
+class Batch
+  def initialize
+    @connectors = JSON.parse(File.read("./connectors.json"))
+  end
+
+  def run
+    @connectors.keys.each do |connector|
+      Generator.new(
+        connector,
+        @connectors[connector]["fivetran_id"],
+        @connectors[connector]["overview"]["description"],
+        @connectors[connector]["body"]["description"]
+      ).generate
+    end
+  end
+end
+
